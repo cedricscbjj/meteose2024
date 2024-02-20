a586bbd2a621174aa072d6afc98f4f1d53151ce6
cedricscbjj <63507246+cedricscbjj@users.noreply.github.com>
2024-02-20 18:47:37 +0100

    Update mainController.js
    ajout du post signup

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,8 +1,11 @@
 
+
+
+
 const router = require('express').Router();
 const fetch = require('node-fetch');
 
-        
+const User = require('../models/user');       
 
 
 const mainController = {
@@ -54,14 +57,7 @@ const mainController = {
 
 //Réception d’un pseudo, d’un email et un password, vérification que le pseudo n’existe pas déjà en base de données, création d’un nouvel utilisateur si le pseudo est bien unique
 
-    tryToRegister: async (req, res, next) => {
-
-
-        console.log("Le controller trytoregister est déclenché ")
-
-
-        res.render("signup", {})
-    },
+   
 //Livraison d’une page affichant un formulaire de d’inscription à un compte utilisateur
     getSignupPage: async (req, res, next) => {
 
@@ -110,6 +106,55 @@ const mainController = {
 
         res.render("faq", {})
     },
+
+
+
+
+
+
+
+
+
+
+
+
+    postSignup: async (req, res, next) => {
+        const { pseudo, email, password } = req.body;
+    
+        console.log("Données reçues du formulaire:", { pseudo, email, password });
+
+        // Save user data to MongoDB
+        try {
+          const newUser = new User({
+            pseudo,
+            email,
+            password, // You should hash the password before saving it in a real-world scenario
+          });
+          await newUser.save();
+          console.log("User information saved to MongoDB:", {
+            pseudo,
+            email,
+            password,
+        });
+          // Redirect or send a response indicating success
+          res.redirect('/login');
+        } catch (error) {
+          // Handle the error, e.g., render an error page
+          console.error(error);
+          res.status(500).send('Internal Server Error');
+        }
+      },
+
+
+      postLogin: async (req, res, next) => {
+        const { pseudo, password } = req.body;
+    
+        console.log("Données envoyées", { pseudo, password });
+
+       
+      },
+
+
 
 
 }
