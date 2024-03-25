fff5462c09a73dd28588e0e08ccc800a23e72d1c
cedricscbjj <63507246+cedricscbjj@users.noreply.github.com>
2024-03-25 18:08:34 +0100

    Update index.js
    MAJ

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 // variables environnement
 require('dotenv').config();
 
-// init de Express
+
 const express = require('express');
 const app = express();
+//app.use(express.urlencoded({ extended: true }))
+const session = require('express-session');
 
+
+app.use(
+  session({
+    secret: '2012keyboardcat66775544',
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+
+
+// determine les assets et dossier statiques
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/app/views');
 app.use(express.static('public'));
@@ -30,10 +44,12 @@ const router = require('./app/router');
 app.use(router);
 
 // ecoute de l'app sur un port
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3009;
 app.listen(PORT, () => {
     console.log(`App Meteose is running or PORT ${PORT}`);
 })
+
+
 
 
 
