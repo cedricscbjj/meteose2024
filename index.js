d96865444b8750a06759325f0e6cb595e02946a4
cedricscbjj <63507246+cedricscbjj@users.noreply.github.com>
2024-07-07 20:56:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-// variables environnement
+
 require('dotenv').config();
-
-
 const express = require('express');
 const app = express();
-//app.use(express.urlencoded({ extended: true }))
 const session = require('express-session');
+//const helmet = require('helmet');
+const path = require('path');
+
+const rateLimit = require('express-rate-limit');
+
+
+
+
+
 
 
 app.use(
@@ -16,13 +22,39 @@ app.use(
   })
 );
 
+const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // Limite chaque IP à 5 requêtes par fenêtre de 15 minutes
+  message: 'Trop de tentatives de connexion à partir de cette adresse IP. Veuillez réessayer après 15 minutes.',
+  headers: true,
+});
+
+
+/*    CA NE FONCTIONNE PAS BIEN MAIS ON GARDE CETTE STRUCTURE AU CAS OU;;;
+  
+app.use(function(req, res, next) {
+
+  res.setHeader("Content-Security-Policy", 
+    `default-src 'self'; ` +
+    `font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com; ` +
+    `style-src 'self' https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/ https://fonts.googleapis.com; ` +
+    `script-src 'self' 'unsafe-inline' https://unpkg.com/@h0rn0chse/night-sky/dist/bundle.min.js https://unpkg.com/feather-icons; ` +
+    `connect-src 'self' https://api.nasa.gov; ` +
+  );
+  next();
+});
+*/
 
 
 // determine les assets et dossier statiques
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/app/views');
-app.use(express.static('public'));
+//app.set('views', __dirname + '/app/views');
+app.set('views', path.join(__dirname, 'app', 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+app.disable('x-powered-by');  // on ne veut pas que express puisse apparaitre en clair  Reduce fingerprinting
 
 
 const mongoose = require('mongoose');
@@ -37,14 +69,17 @@ db.once('open', () => {
 });
 
 
-
+app.get('/sitemap.xml', (req, res) => {
+  const filePath = path.join(__dirname, 'public', 'sitemap.xml');
+  res.sendFile(filePath);
+});
 
 // mise en place des routes et des controllers
 const router = require('./app/router');
 app.use(router);
 
 // ecoute de l'app sur un port
-const PORT = process.env.PORT || 3009;
+const PORT = process.env.PORT || 3017;
 app.listen(PORT, () => {
     console.log(`App Meteose is running or PORT ${PORT}`);
 })
