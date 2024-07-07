7c9c123f2dda4a9769abeb11c9023287a4cdb2cd
cedricscbjj <63507246+cedricscbjj@users.noreply.github.com>
2024-07-07 20:58:36 +0100

    Update router.js

--- app/router.js
+++ app/router.js
@@ -1,11 +1,18 @@
-
-
 // init de Express et du router
 const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch');
 require('dotenv').config()
 
+const rateLimit = require('express-rate-limit');
+
+// Définir la limitation de taux pour la route de login
+const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // Limite chaque IP à 5 requêtes par fenêtre de 15 minutes
+  message: 'Trop de tentatives de connexion à partir de cette adresse IP. Veuillez réessayer après 15 minutes.',
+  headers: true,
+});
 const mainController = require('./controllers/mainController');
 
 router.use(express.json());
@@ -14,20 +21,24 @@ router.get('/', mainController.homePage);
 router.get('/login', mainController.getLoginPage);
 router.get('/signup', mainController.getSignupPage);
 router.get('/cityResults', mainController.getCityresultpage);
-router.get('/aboutUs', mainController.getaboutus);
+router.get('/decla', mainController.decla);
 router.get('/faq', mainController.getfaq);
-router.get('/pagesecrete', mainController.getSecretpage);
 router.get('/nasa', mainController.getNasapage);
+
+router.get('/astrogame', mainController.getAstropage);
+
 router.get('/index2', mainController.getindex2page);
 router.get('/forbidden', mainController.getforbiddenpage);
 router.get('/profil', mainController.getprofilpage);
-router.get('/logout', mainController.getLogout);
 
 router.post('/signup', mainController.postSignup); 
-router.post('/login', mainController.tryToLogin);
+router.post('/login', loginLimiter, mainController.tryToLogin);
+router.get('/logout', mainController.getLogout);
+router.get('/rgpd', mainController.getRgpd);
+//router.get('/renderView', mainController.renderView);
 
 
-
+router.delete('/profil', mainController.tryToDelete);
 /*router.get('/', (req, res) => {
   res.render('home', {
     city: null,
